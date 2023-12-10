6c932aea8028ed096068e008aa374c5923437600
root <sebastianvarg11@gmail.com>
2023-12-10 18:56:50 -0500

    up

--- src/modules/novelty/novelty.module.ts
+++ src/modules/novelty/novelty.module.ts
@@ -5,11 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Novelty, NoveltySchema } from './entities/novelty.entity';
 import { NoveltyController } from './controllers/novelty.controller';
 import {Counter, CounterSchema} from "./entities/counter.entity";
+import {Concept, ConceptSchema} from "../concepts/entities/concepts.entity";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Novelty.name, schema: NoveltySchema }]),
     MongooseModule.forFeature([{ name: Counter.name, schema: CounterSchema }]),
+    MongooseModule.forFeature([{ name: Concept.name, schema: ConceptSchema }]),
   ],
   controllers: [NoveltyController],
   providers: [NoveltyService],
