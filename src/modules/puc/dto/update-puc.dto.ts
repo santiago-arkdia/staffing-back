cc0ee676792be2f35acb2b03cf8f27a3d1a3f59c
roger2016 <cto@arkdia.co>
2024-05-15 22:56:16 -0500

    client+

--- src/modules/puc/dto/update-puc.dto.ts
+++ src/modules/puc/dto/update-puc.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from "@nestjs/swagger";
 import { CreatePucDto } from "./create-puc.dto";
+import { IsNumber, IsOptional } from "class-validator";
 
-export class UpdatePucDto extends PartialType (CreatePucDto) {}
+export class UpdatePucDto extends PartialType (CreatePucDto) {
+
+    @IsNumber()
+    @IsOptional()
+    state: number;
+
+}
